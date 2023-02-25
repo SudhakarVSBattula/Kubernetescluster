4742195c7d2ea1f210ef015dcc4ab71fccb7e98a
SudhakarVSBattula <sudhakarbvs@outlook.com>
2023-02-25 23:48:29 +0530

    New Deployment

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_kubernetes_cluster" "aksclusterref" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 3
     vm_size    = "Standard_D2_v2"
   }
 
