b0ff2c0593066aaf16ce112a08b2c838650b1a79
SudhakarVSBattula <sudhakarbvs@outlook.com>
2023-02-25 23:02:20 +0530

    Changed ACR Name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "akskubergref" {
 }
 # Creating Azure Container Registry inside resource group
 resource "azurerm_container_registry" "acraksref" {
-  name                = "acraks"
+  name                = "acrakscontainer"
   resource_group_name = azurerm_resource_group.akskubergref.name
   location            = azurerm_resource_group.akskubergref.location
   sku                 = "Basic"
